993308532fffe68994f4222a24bc072e72b18c5d
hzc <hzchao@123.com>
2025-08-20 15:22:32 +0800

    refactor: 解决推送代码自动部署时的报错问题，优化Prisma配置并更新依赖项
    - 移除schema.prisma中的output配置，使用默认路径
    - 更新db.ts中的PrismaClient导入路径
    - 在package.json中添加postinstall脚本并调整依赖项位置
    - 为主页添加导航链接到admin页面

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import Link from "next/link";
 export default async function page() {
   return (
-    <>111</>
+    <div className="flex h-screen w-full items-center justify-center">
+      <Link href="/admin">
+        <h1 className="cursor-pointer text-green-400 underline">
+          click visit admin page
+        </h1>
+      </Link>
+    </div>
   );
 }

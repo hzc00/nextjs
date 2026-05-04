e5f3c4fcc8ae1dd8b9a87bda33400714c04b0b47
DESKTOP-8OTQ7KS\GIGABYTE <zhangchao9958@gmail.com>
2026-05-04 16:19:51 +0800

    chore: initialize root layout with Google Analytics, site metadata, and global providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/providers";
 import { Metadata } from "next";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,6 +20,9 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -26,6 +30,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-DTTWN9Y9CB"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-DTTWN9Y9CB');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
